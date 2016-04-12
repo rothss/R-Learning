33b91d5be1921d61faff33847905702c319ba0b9
rothss <rothss@outlook.com>
2016-04-12 09:48:04 +0800

    添加注释

--- FAEX2Ability.R
+++ FAEX2Ability.R
@@ -1,14 +1,14 @@
-setwd("C:\\Users\\roths\\Documents\\多变量分析\\Sec03 PCA and FA\\FA")
+setwd("#Ability.txt所在文件夹,\需替换为\\")
 ability <- as.matrix(read.table("Ability.txt"))
 
 library(corrplot)
 library(GPArotation)
-corrplot(ability,order= "hclust")
+corrplot(ability,order= "hclust")#粗浅的通过图来判断因子个数
 
-library(psych)
+library(psych)#提供比R自带的factanal更好的因子分析函数fa
 cortest.bartlett(ability,n=556)
 fa.parallel(ability,n.obs=556,fa="both",n.iter = 100,
-            main = "Screen plots with parallel analysis")
+            main = "Screen plots with parallel analysis")#一般选择折点的横坐标作为因子个数
 
 NumFA=2
 fa <- fa(ability,nfactors = 2,rotate="promax",fm="ml",scores="regression",residuals=TRUE)
