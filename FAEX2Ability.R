e3ad471a90be24aaa7865b2dfce7b0d0253439b6
rothss <rothss@outlook.com>
2016-04-12 09:41:07 +0800

    FA(Using psych)
    多变量分析课程代码之因子分析

---
+++ FAEX2Ability.R
@@ -0,0 +1,48 @@
+setwd("C:\\Users\\roths\\Documents\\多变量分析\\Sec03 PCA and FA\\FA")
+ability <- as.matrix(read.table("Ability.txt"))
+
+library(corrplot)
+library(GPArotation)
+corrplot(ability,order= "hclust")
+
+library(psych)
+cortest.bartlett(ability,n=556)
+fa.parallel(ability,n.obs=556,fa="both",n.iter = 100,
+            main = "Screen plots with parallel analysis")
+
+NumFA=2
+fa <- fa(ability,nfactors = 2,rotate="promax",fm="ml",scores="regression",residuals=TRUE)
+fa
+fa.varimax <- fa(ability,nfactors=2,rotate="varimax",fm="ml")
+fa.varimax
+fa.promax <- fa(ability,nfactors=2,rotate="promax",fm="pa")
+fa.promax
+
+sapply(1:NumFA, function(f) factanal(covmat=ability,n.obs=556, factors = f, method ="mle")$PVAL)
+
+FAmle1=factanal(covmat=ability,n.obs=556, factors = 2, method ="mle", rotation = "promax")#only mle can be used in factanal
+FAmle2= fa(ability, nfactors = 2,fm = "ml", rotate = "varimax", scores="regression",residuals=TRUE)
+FAmle1
+FAmle2
+
+Score1 <- FAmle1$scores #no scoare is available if covmat is used
+
+FAmle1$uniquenesses
+(FAmle1.communality=1-FAmle1$uniquenesses)
+
+URFAmle1=factanal(covmat=ability,n.obs=556,factors = 2, method ="mle", rotation = "none")#only mle can be used in factanal
+#URFAmle2 <- fa(life, nfactors = 3,fm = "ml", rotate = "none", scores="regression")
+
+plot(URFAmle1$loadings[,c(1:2)], pch = as.character(c(1:dim(ability)[2])),
+     xlab = expression(paste(gamma,"1")), ylab = expression(paste(gamma,"2")),
+     main = "First and second loadings",
+     xlim = c(-1,1), ylim = c(-1,1))
+points(FAmle1$loadings[,c(1:2)],
+       pch = letters[c(1:dim(ability)[2])], col = "red")
+abline(h = 0)
+abline(v = 0)
+
+library(semPlot)
+semPaths(FAmle1,what="est",residuals=F,cut=0.4,posCol=c("white","darkgreen"),negCol=c("white","red"),nCharNodes = 4)
+semPaths(fa,what="est",residuals=F,cut=0.4,posCol=c("white","darkgreen"),negCol=c("white","red"),nCharNodes = 4)
+semPaths(fa.varimax,what="est",residuals=F,cut=0.4,posCol=c("white","darkgreen"),negCol=c("white","red"),nCharNodes = 4)
